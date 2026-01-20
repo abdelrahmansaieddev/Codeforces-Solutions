100b525a32f78a40f55091dc42a88e4b1f0c2a9b
abdelrahman saied <abdelrahman.saied.dev@gmail.com>
2026-01-20 04:41:34 +0200

    Add soluation for sheet assiut

---
+++ Assiut University Training - Newcomers/Sheet-01-Data-type-Conditions/(D) Difference.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+    long long num1, num2, num3, num4, sum = 0;
+    cin >> num1 >> num2 >> num3 >> num4;
+    sum = (num1 * num2) - (num3 * num4);
+    cout << "Difference = " << sum << endl;
+    return 0;
+}
